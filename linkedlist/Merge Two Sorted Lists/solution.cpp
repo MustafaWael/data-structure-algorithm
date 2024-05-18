e07226e80236b516a3e8a5a35b5cb7b264f843fc
MustafaWael <mustafa.wael.dev>
2024-05-18 17:01:07 +0300

    chore: Implement mergeTwoLists function for merging two sorted linked lists

---
+++ linkedlist/Merge Two Sorted Lists/solution.cpp
@@ -0,0 +1,52 @@
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* dummy = new ListNode(-1);
+    ListNode* current = dummy;
+
+    while (list1 && list2) {
+      if (list1->val <= list2->val) {
+        current->next = list1;
+        list1 = list1->next;
+      }
+      else {
+        current->next = list2;
+        list2 = list2->next;
+      }
+      current = current->next;
+    }
+
+    // Append remaining nodes
+    if (list1) {
+      current->next = list1;
+    }
+    else {
+      current->next = list2;
+    }
+
+    ListNode* merged = dummy->next;
+    delete dummy; // free the dummy node
+    return merged;
+
+  }
+
+};
+
+
+int main() {
+  ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(4, new ListNode(5, new ListNode(6)))));
+  ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+  Solution solution;
+  ListNode* merged = solution.mergeTwoLists(list1, list2);
+
+  return 1;
+}
